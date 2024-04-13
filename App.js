edb7eaab43cfb39cda15b84710ac37682eb2d5f4
Suhas Palani <spalani3@hawk.iit.edu>
2024-04-12 19:40:55 -0500

    view style

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableNativeFeedback,SafeAreaView } from 'react-native';
 
 export default function App() {
   
   
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
-      
-      <Image 
-      blurRadius={5}
-      fadeDuration={1000}
-      // resizeMode='center'
-      source={{
-        width:200,
-        height:300,
-        uri: "https://picsum.photos/200/300"}} />
+      <Text >Hello React Native!</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
+      <View style={{ width:200,height:70,backgroundColor:"dodgerblue" }}></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
