eafc2d3bb1115bda515d361480e9de0ecb77d6d2
Suhas Palani <spalani3@hawk.iit.edu>
2024-04-12 19:45:01 -0500

    button

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableNativeFeedback,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableNativeFeedback,SafeAreaView, Button } from 'react-native';
 
 export default function App() {
   
   
   return (
     <SafeAreaView style={styles.container}>
-      <Text >Hello React Native!</Text>
+      {/* <Text >Hello React Native!</Text>
       <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
       <View style={{ width:200,height:70,backgroundColor:"dodgerblue" }}></View>
       </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
+
+      <Button title="Click Me" color="orange" onPress={() => console.log("Button Tapped")}/>
     </SafeAreaView>
   );
 }
