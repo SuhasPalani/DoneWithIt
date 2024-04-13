94296753a59bb98144901bcc902d88ab6077288a
Suhas Palani <spalani3@hawk.iit.edu>
2024-04-13 04:03:09 -0500

    justify and align

--- App.js
+++ App.js
@@ -12,7 +12,18 @@ export default function App() {
     <View style={{
       backgroundColor: '#fff',
       flex: 1,
-      flexDirection: 'row',
+      flexDirection: 'row', //horizontal
+      justifyContent: 'center', //main axis
+      // alignItems: "baseline",
+      alignItems: "center",
+      // alignItems: "flex-end",
+      // alignItems: "flex-start",
+      // alignItems: "stretch",
+      // justifyContent: 'flex-end', //main axis
+      // justifyContent: 'flex-start', //main axis
+      // justifyContent: 'space-around', //main axis
+      // justifyContent: 'space-between', //main axis
+      // justifyContent: 'space-evenly', //main axis
       // flexDirection: 'column',
       // flexDirection: 'column-reverse',
       // flexDirection: 'row-reverse',
@@ -20,12 +31,13 @@ export default function App() {
       <View style={{
         backgroundColor: 'dodgerblue',
         width: 100,
-        height: 100
+        height: 300,
+        alignSelf: 'flex-start'
       }}/>
       <View style={{
         backgroundColor: 'gold',
         width: 100,
-        height: 100
+        height: 200
       }}/>
       <View style={{
         backgroundColor: 'tomato',
